dfe7f367ae565e8178d7a157bb2aad544d6fcfd0
Griffeth Barker <gbarker@bhwk.com>
2024-08-31 17:41:11 -0700

    Remove unused parameters

--- obsidianmd-powershell/public/Get-OMDVaultPath.ps1
+++ obsidianmd-powershell/public/Get-OMDVaultPath.ps1
@@ -5,13 +5,10 @@
  .Description
  Gets the vaule of the $env:OMDVaultPath environment variable.
 
- .Parameter Reload
- A switch that will reload the current user's PowerShell profile to make the $env:OMDVaultPath variable available.
-
  .Example
- Remove-OMDVaultPath
+ Get-OMDVaultPath
 
- Removes any declared path to your Obsidian vault.
+ Gets the declared path to your Obsidian vault.
 #>
 
 function Get-OMDVaultPath {
