7a22edd78381896275ee613571946a3edb126b5c
Griffeth Barker <gbarker@bhwk.com>
2024-09-01 15:36:19 -0700

    Rename cmdlet and add help

--- obsidianmd-powershell/public/Append-OMDVaultNoteContent.ps1
+++ obsidianmd-powershell/public/Append-OMDVaultNoteContent.ps1
@@ -8,19 +8,33 @@
  .Parameter Name
   A string containing the name of the note to which you'd like to append content.
   
- .Parameter Relative Path
+ .Parameter RelativePath
   A string containing the relative path to the directory in your vault containing the note to which you'd like to append content.
   
- .Parameter Full Path
+ .Parameter FullPath
   A string containing the full path to the directory containing the note to which you'd like to append content.
   
  .Parameter Content
   A string containing the content you'd like to append to the end of the specified note.
-
  .Example
   Append-OMDVaultNote -Name 'How to create a new note' -Content 'This is some content to append to the end of the note.'
 
-  Searches for notes in your Obsidian vault whose file name or full path name contain the word 'Linux'.
+  Appends 'This is some content to append to the end of the note.' to the 'How to create a new note' note in the top-level directory of your Obsidian vault.
+
+ .Example
+  Append-OMDVaultNote -Name 'How to create a new note' -Content 'This is some content to append to the end of the note.' -RelativePath 'daily_notes\september'
+
+  Appends 'This is some content to append to the end of the note.' to the 'How to create a new note' note in the 'daily_notes\september' directory of your Obsidian vault.
+ 
+  .Example
+  Append-OMDVaultNote -Name 'How to create a new note' -Content 'This is some content to append to the end of the note.' -FullPath 'C:\Users\john.smith\Documents\Obsidian\daily_notes\september'
+
+  Appends 'This is some content to append to the end of the note.' to the 'How to create a new note' note at the specified path inside your Obsidian vault.
+
+ .Example
+  Append-OMDVaultNote -Name 'How to create a new note' -Content 'This is some content to append to the end of the note.' -FullPath 'C:\Users\john.smith\Documents\Obsidian\daily_notes\september'
+
+  Appends 'This is some content to append to the end of the note.' to the 'How to create a new note' note in the top-level directory of your Obsidian vault.
 #>
 
 function Append-OMDVaultNoteContent {
