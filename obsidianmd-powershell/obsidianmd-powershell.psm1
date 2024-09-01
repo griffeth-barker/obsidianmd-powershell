5f57dea6eaa2b5df419f38702a8840f534e5ea5f
Griffeth Barker <gbarker@bhwk.com>
2024-09-01 16:14:42 -0700

    FunctionsToExport default null value

--- obsidianmd-powershell/obsidianmd-powershell.psm1
+++ obsidianmd-powershell/obsidianmd-powershell.psm1
@@ -69,16 +69,7 @@
   # NestedModules = @()
   
   # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-  FunctionsToExport = @(
-    "Append-OMDVaultNoteContent",
-    "Get-OMDVaultNote",
-    "Get-OMDVaultNoteContent",
-    "Get-OMDVaultPath",
-    "New-OMDVaultNote",
-    "Remove-OMDVaultNote",
-    "Remove-OMDVaultPath",
-    "Set-OMDVaultPath"
-  )
+  FunctionsToExport = @()
   
   # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
   CmdletsToExport = @()
